8056c72b3b30520928d4bffc6e4329604f4eb85c
NVZGUL <h3rmess@gmail.com>
2018-02-16 15:41:11 +0300

    added simple query to db

--- data/psqlstore.go
+++ data/psqlstore.go
@@ -1,25 +1,59 @@
 package data
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
+
+	"github.com/go-pg/pg"
 )
 
 type PsqlStore struct {
-	session *sql.DB
+	db *pg.DB
 }
 
-func NewPsqlStore(connection string) {
-	session, err := sql.Open("postgres", connection)
+type Subject struct {
+	Id       int
+	CourseId int
+	Name     string
+}
+
+type Course struct {
+	Id   int
+	Name string
+}
+
+func NewPsqlStore(dbName string) (*PsqlStore, error) {
+	db := pg.Connect(&pg.Options{
+		User:     "root",
+		Password: "root",
+		Database: dbName,
+	})
+	return &PsqlStore{db: db}, nil
+}
+
+func (p *PsqlStore) Test() {
+	var n int
+	_, err := p.db.Query(pg.Scan(&n), "SELECT ?", 42)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Errorf(err.Error())
 	}
-	rows, err := session.Query("SELECT 1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(rows)
-	defer session.Close()
-	// return &PsqlStore{session: session}, nil
+	fmt.Println(n)
+}
+
+func (p *PsqlStore) CreateCourse(course *Course) error {
+	_, err := p.db.QueryOne(course, `
+		INSERT INTO courses (name) VALUES (?name)
+		RETURNING name`, course)
+	return err
+}
+
+func (p *PsqlStore) GetCourses() ([]Course, error) {
+	var courses []Course
+	_, err := p.db.Query(&courses, `SELECT * FROM courses`)
+	return courses, err
+}
+
+func (p *PsqlStore) GetCourse(id string) (Course, error) {
+	var course Course
+	_, err := p.db.QueryOne(&course, `SELECT * FROM courses WHERE id=?`, id)
+	return course, err
 }
