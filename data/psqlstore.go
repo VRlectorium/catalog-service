1e4143acdbfd148f724c466ade30088f05a00539
NVZGUL <h3rmess@gmail.com>
2018-03-02 18:19:56 +0300

    updated course and subjects structs, updated get courses and added post and get subjects methids

--- data/psqlstore.go
+++ data/psqlstore.go
@@ -13,13 +13,14 @@ type PsqlStore struct {
 
 type Subject struct {
 	Id       int
-	CourseId int
+	Courseid int
 	Name     string
 }
 
 type Course struct {
-	Id   int
-	Name string
+	Id       int
+	Name     string
+	Subjects []Subject
 }
 
 func NewPsqlStore(dbName string) (*PsqlStore, error) {
@@ -58,9 +59,36 @@ func (p *PsqlStore) CreateCourse(course *Course) error {
 	return err
 }
 
+type ORMres struct {
+	Id        int
+	Name      string
+	Subject   string
+	Subjectid int
+}
+
 func (p *PsqlStore) GetCourses() ([]Course, error) {
 	var courses []Course
-	_, err := p.db.Query(&courses, `SELECT * FROM courses`)
+	var res []ORMres
+	_, err := p.db.Query(&res,
+		`SELECT courses.id, courses.name, subjects.name as subject, subjects.id as subjectid
+		FROM courses INNER JOIN subjects ON courses.id=subjects.courseid`)
+	for _, r := range res {
+		if len(courses) > r.Id-1 && courses[r.Id-1].Id == r.Id {
+			courses[r.Id-1].Subjects = append(courses[r.Id-1].Subjects, Subject{
+				Id:       r.Subjectid,
+				Courseid: r.Id,
+				Name:     r.Subject})
+		} else {
+			course := Course{
+				Id:   r.Id,
+				Name: r.Name,
+				Subjects: []Subject{Subject{
+					Id:       r.Id,
+					Courseid: r.Subjectid,
+					Name:     r.Subject}}}
+			courses = append(courses, course)
+		}
+	}
 	return courses, err
 }
 
@@ -68,4 +96,23 @@ func (p *PsqlStore) GetCourse(id string) (Course, error) {
 	var course Course
 	_, err := p.db.QueryOne(&course, `SELECT * FROM courses WHERE id=?`, id)
 	return course, err
+}
+
+func (p *PsqlStore) CreateSubject(subject *Subject) error {
+	_, err := p.db.QueryOne(subject, `
+		INSERT INTO subjects (courseid, name) VALUES (?courseid,?name)
+		RETURNING name`, subject)
+	return err
+}
+
+func (p *PsqlStore) GetSubjects() ([]Subject, error) {
+	var subjects []Subject
+	_, err := p.db.Query(&subjects, `SELECT * FROM subjects`)
+	return subjects, err
+}
+
+func (p *PsqlStore) GetSubject(id string) (Subject, error) {
+	var subject Subject
+	_, err := p.db.QueryOne(&subject, `SELECT * FROM subjects WHERE id=?`, id)
+	return subject, err
 }
